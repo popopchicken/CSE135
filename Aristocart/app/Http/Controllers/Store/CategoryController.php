9c6c505cbcce22cf319eaf815f04dba4d243a12e
Tyler <ttedeschi@ucsd.edu>
2015-04-21 18:09:38 -0700

    Fixed forms by adding token input

---
+++ Aristocart/app/Http/Controllers/Store/CategoryController.php
@@ -0,0 +1,44 @@
+<?php namespace App\Http\Controllers;
+
+use App\HTTP\Requests\LoginFormRequest;
+use Response;
+use View;
+
+class CategoryController extends Controller {
+
+	/*
+	|--------------------------------------------------------------------------
+	| Category Controller
+	|--------------------------------------------------------------------------
+	|
+	| This controller renders the Category page, and allows the owners to 
+	| create new categories, but hides the input for customers.
+	|
+	*/
+
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		$this->middleware('guest');
+	}
+
+	/**
+	 * Show the sign up screen to the user.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		return view('store/categories');
+	}
+
+	public function addCategories()
+	{
+		return view('store/categories');
+	}
+
+}
---
+++ Aristocart/resources/views/store/categories.blade.php
@@ -0,0 +1,25 @@
+<html>
+	<head>
+		<title>Categories</title>
+	</head>
+	<body>
+		<h1>Categories</h1>
+		<br />
+		<br />
+		<a href="#">Category 1</a>
+		<br />
+		<br />
+		<a href="#">Category 2</a>
+		<br />
+		<br />
+		<a href="#">Category 3</a>
+		<br />
+		<br />
+		<form method="POST" role="form" action="{{ url('store/categories')}}">
+			<input type="hidden" name="_token" value="{{ csrf_token() }}">
+			<label for="add_category">
+			<input type="text" name="add_category">
+			<input type="submit" value="Add Category">
+		</form>
+	</body>
+</html>
