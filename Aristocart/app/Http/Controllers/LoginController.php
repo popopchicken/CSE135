c64e597ab88eb5d6170bfdafe03a36a5a7c0d422
Tyler <ttedeschi@ucsd.edu>
2015-04-26 15:42:25 -0700

    Update for initial pull

--- Aristocart/app/Http/Controllers/LoginController.php
+++ Aristocart/app/Http/Controllers/LoginController.php
@@ -1,6 +1,6 @@
 <?php namespace App\Http\Controllers;
 
-use App\HTTP\Requests\LoginFormRequest;
+use Request;
 use App\Models\User;
 use Response;
 use View;
@@ -40,15 +40,16 @@ class LoginController extends Controller {
 	public function signUp()
 	{
 		//TODO: Logic for signing up needs to be added here.
+		$user_name = Request::input('user_name');
+
 		return view('sign-up');
 	}
 
 	public function login()
 	{
 		$user = new User();
-		$user->name = 'Tyler';
-		$user->age = 22;
-		$user->state_id = 1;
+		//TODO: Add user data here
+		//Ex: $user->name = "Tyler";
 		$user->addUser();
 		//TODO: Logic for logging in needs to be added here.
 		return view('store/categories');
--- Aristocart/app/Models/User.php
+++ Aristocart/app/Models/User.php
@@ -1,15 +1,15 @@
 <?php namespace App\Models;
 
 use  Illuminate\Database\DatabaseManager;
-
+use DB;
 
 class User{
-	public $name;
-	public $age;
-	public $state_code;
+	public $name = '';
+	public $age = 0;
+	public $state_id = 0;
 
 	public function addUser(){
-		$results = DB::insert('insert into users (name, age, state) values(?,?)', [$name, $age, $state_code]);
+		$results = DB::insert('insert into users (name, age, state_id) values(?,?,?)', [$name, $age, $state_id]);
 	}
 
 	public function doesUserExist(){
